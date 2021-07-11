0603debd914e7f9a196a22f3109f41025db2f796
Valentin Vakrilov <vvakrilov@gmail.com>
2021-07-11 18:28:48 +0300

    homework update

---
+++ Basics/03 Nested Conditional Statements/01. Lab/01. Day of Week.js
@@ -0,0 +1,27 @@
+function solve(values_input) {
+  given_number = Number(values_input[0]) - 1;
+
+  const daysOfWeek = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+
+  if (typeof daysOfWeek[given_number] !== 'undefined') {
+    console.log(`${daysOfWeek[given_number]}`);
+  } else {
+    console.log(`Error`);
+  }
+}
+// solve(["1"]);
+// solve(["2"]);
+// solve(["3"]);
+// solve(["4"]);
+// solve(["5"]);
+// solve(["6"]);
+// solve(["7"]);
+// solve(["-1"]);
